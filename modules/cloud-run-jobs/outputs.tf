c58e2f64750e4288ceaa9009b12191f5b2965432
Joseph Selvin <Joseph@intelligencebusinesssolutions.com>
2026-01-10 13:07:43 +0000

    added cloud run job

--- modules/cloud-run-jobs/outputs.tf
+++ modules/cloud-run-jobs/outputs.tf
@@ -1,4 +1,4 @@
 output "cloud_run_job_id" {
   description = "The unique identifier for the Job"
-  value       = google_cloud_run_v2_job.data_processor.id
+  value       = google_cloud_run_v2_job.cloud_run_job.id
 }
