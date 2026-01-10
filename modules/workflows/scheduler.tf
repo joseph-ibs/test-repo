be462a8a7b4424757e07d16e7058587a4023b68e
Joseph Selvin <Joseph@intelligencebusinesssolutions.com>
2026-01-10 12:47:52 +0000

    changed location

--- modules/workflows/scheduler.tf
+++ modules/workflows/scheduler.tf
@@ -7,7 +7,7 @@ resource "google_cloud_scheduler_job" "workflow_job" {
   http_target {
     http_method = "POST"
 
-    uri = "https://workflowexecutions.googleapis.com/v1/projects/${var.project_id}/locations/us-central1/workflows/${var.workflow_name}/executions"
+    uri = "https://workflowexecutions.googleapis.com/v1/projects/${var.project_id}/locations/${var.region}/workflows/${var.workflow_name}/executions"
 
     oauth_token {
       service_account_email = google_service_account.workflow_service_account.email
